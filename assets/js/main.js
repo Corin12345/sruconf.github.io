5effdb9decb7106bdaa7793c78e74ba087d2f1e3
Corin12345 <105778170+Corin12345@users.noreply.github.com>
2022-12-26 17:45:33 +0530

    Update date

--- assets/js/main.js
+++ assets/js/main.js
@@ -149,7 +149,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "09/30/",
+      dayMonth = "04/10/",
       birthday = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
